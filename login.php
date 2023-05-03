5fd04a746908ac561e4a766fe116dbac0917003b
dayen <nubicodes@gmail.com>
2023-05-03 14:07:42 +0700

    limit maxlength and another update

--- login.php
+++ login.php
@@ -17,21 +17,22 @@ $twt->login();
 
 <head>
     <meta charset="UTF-8">
-    <title>Sign Up Sign in Form Template Example</title>
+    <title>Register/Login Page</title>
     <link rel="stylesheet" href="css/login.css">
-    <!-- <link rel="stylesheet" href="css/login.css?php echo time(); ?>"> -->
+    <link rel="icon" href="img/logo.png">
+    <script src="sweetalert2.min.js"></script>
+    <link rel="stylesheet" href="sweetalert2.min.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
 </head>
 
 <body>
     <!-- partial:index.partial.html -->
     <html lang="en">
 
-    <head>
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    </head>
-
     <body>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
         <div id="form">
             <div class="container">
                 <div class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-md-8 col-md-offset-2">
@@ -50,7 +51,7 @@ $twt->login();
                                                 <label>First Name<span class="req">*</span> </label>
                                                 <input type="text" name="firstname" class="form-control" id="first_name"
                                                     required data-validation-required-message="Please enter your name."
-                                                    autocomplete="off">
+                                                    autocomplete="off" maxlength="10">
                                                 <p class="help-block text-danger"></p>
                                             </div>
                                         </div>
@@ -59,10 +60,24 @@ $twt->login();
                                                 <label> Last Name<span class="req">*</span> </label>
                                                 <input type="text" name="lastname" class="form-control" id="last_name"
                                                     required data-validation-required-message="Please enter your name."
-                                                    autocomplete="off">
+                                                    autocomplete="off" maxlength="20">
                                                 <p class="help-block text-danger"></p>
                                             </div>
                                         </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label> Your Nickname<span class="req">*</span> </label>
+                                        <input type="text" name="nickname" class="form-control" id="nickname" required
+                                            data-validation-required-message="Please enter your nickname."
+                                            autocomplete="off" maxlength="20">
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="form-group">
+                                        <label> Your Username<span class="req">*</span> </label>
+                                        <input type="text" name="username" class="form-control" id="username" required
+                                            data-validation-required-message="Please enter your username."
+                                            autocomplete="off" maxlength="12">
+                                        <p class="help-block text-danger"></p>
                                     </div>
                                     <div class="form-group">
                                         <label> Your Email<span class="req">*</span> </label>
@@ -71,24 +86,27 @@ $twt->login();
                                             autocomplete="off">
                                         <p class="help-block text-danger"></p>
                                     </div>
-                                    <div class="form-group">
-                                        <label> Your Username<span class="req">*</span> </label>
-                                        <input type="text" name="username" class="form-control" id="username" required
-                                            data-validation-required-message="Please enter your username."
-                                            autocomplete="off">
-                                        <p class="help-block text-danger"></p>
-                                    </div>
-                                    <div class="form-group">
-                                        <label> Password<span class="req">*</span> </label>
-                                        <input type="password" name="password" class="form-control" id="password"
-                                            required data-validation-required-message="Please enter your password"
-                                            autocomplete="off">
-                                        <p class="help-block text-danger"></p>
-                                        <label> Confirm Password<span class="req">*</span> </label>
-                                        <input type="password" name="password2" class="form-control" id="password"
-                                            required data-validation-required-message="Please enter your password"
-                                            autocomplete="off">
-                                        <p class="help-block text-danger"></p>
+                                    <div class="row">
+                                        <div class="col-xs-12 col-sm-6">
+                                            <div class="form-group">
+                                                <label> Password<span class="req">*</span> </label>
+                                                <input type="password" name="password" class="form-control"
+                                                    id="password" required
+                                                    data-validation-required-message="Please enter your password"
+                                                    autocomplete="off">
+                                                <p class="help-block text-danger"></p>
+                                            </div>
+                                        </div>
+                                        <div class="col-xs-12 col-sm-6">
+                                            <div class="form-group">
+                                                <label> Confirm Password<span class="req">*</span> </label>
+                                                <input type="password" name="password2" class="form-control"
+                                                    id="password" required
+                                                    data-validation-required-message="Please enter your password"
+                                                    autocomplete="off">
+                                                <p class="help-block text-danger"></p>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div class="mrgn-30-top">
                                         <button type="submit" name="register" class="btn btn-larger btn-block" />
@@ -104,7 +122,7 @@ $twt->login();
                                         <label for="username"> Username<span class="req">*</span> </label>
                                         <input type="text" name="username" class="form-control" id="username" required
                                             data-validation-required-message="Please enter your username"
-                                            autocomplete="off">
+                                            autocomplete="off" maxlength="12">
                                         <p class="help-block text-danger"></p>
                                     </div>
                                     <div class="form-group">
