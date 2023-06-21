0f0d70561ffd773210c255ca16b646e0dc733129
dayen <nubicodes@gmail.com>
2023-06-21 17:30:38 +0700

    add config, change the constant

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php 
+define('BASEURL', 'http://localhost/tweetify/public');
+
+// Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'tweetify');
+
+// App Name
+// Pengaplikasian variable final/const
+define('APP_NAME', 'Tweetify');
+define('APP_VERSION', '1.0.0');
+define('APP_AUTHOR', 'Dian Saputra');
+define('APP_URL', 'tweetify.my.id');
+define('APP_DESCRIPTION', 'Tweetify adalah aplikasi berbasis web yang dibuat untuk memenuhi tugas akhir mata kuliah Pemrograman Berorientasi Objek.');
+
+?>
