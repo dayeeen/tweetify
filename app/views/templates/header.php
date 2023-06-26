3a784a599f9a53ef6938f42d0d424af02aa3a139
dayen <nubicodes@gmail.com>
2023-06-26 17:54:00 +0700

    update templates

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,5 +1,4 @@
-<?php 
-require "../app/lib/functions.php";
+<?php
 ?>
 
 <!DOCTYPE html>
@@ -11,18 +10,14 @@ require "../app/lib/functions.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <!-- JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <!-- CSS 2 -->
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/<?= $data['css']; ?>">
     <link rel="icon" href="img/logo.png">
-    <title><?= $data['judul']; ?></title>
+    <title>
+        <?= $data['judul']; ?>
+    </title>
 </head>
 
 <body>
